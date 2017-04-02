9e9b8c6a46be5e7d77a724025a631d47c29572c4
Glen Knight <glenak1911@gmail.com>
2017-04-02 02:13:28 -0400

    modified front page layout

--- front-page.php
+++ front-page.php
@@ -1,19 +1,41 @@
 <?php get_header(); ?>
-
+<br />
   <div class="row">
-    <div class="col-md-6">
-      Little notes
+    <div class="col-md-2">
     </div>
-    <div class="col-md-6">
-      best thoughts @ 3am
+    <div class="col-md-3">
+      <div class="frontPageSection">
+        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/little-notes/">Little notes</a></h3>
+      </div>
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-3">
+      <div class="frontPageSection">
+        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/best-thoughts/">Best thoughts @ 3am</a></h3>
+      </div>
+    </div>
+    <div class="col-md-2">
     </div>
   </div>
+  <br />
+  <br />
   <div class="row">
-    <div class="col-md-6">
-      Currently Consuming
+    <div class="col-md-2">
     </div>
-    <div class="col-md-6">
-      Obsessions
+    <div class="col-md-3">
+      <div class="frontPageSection">
+        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/currently-consuming/">Currently Consuming</a></h3>
+      </div>
+    </div>
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-3">
+      <div class="frontPageSection">
+        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/obsessions/">Obsessions</a></h3>
+      </div>
+    </div>
+    <div class="col-md-2">
     </div>
   </div>
 
