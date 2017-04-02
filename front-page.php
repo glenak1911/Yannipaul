29efaf7d723101b446072ffa2a20190f50496ac1
Glen Knight <glenak1911@gmail.com>
2017-04-02 02:23:41 -0400

    modified front page containers

--- front-page.php
+++ front-page.php
@@ -1,40 +1,28 @@
 <?php get_header(); ?>
   <div class="row">
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-3">
+    <div class="col-md-6">
       <div class="frontPageSection">
         <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/little-notes/">Little notes</a></h3>
       </div>
     </div>
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-3">
+    <div class="col-md-6">
       <div class="frontPageSection">
         <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/best-thoughts/">Best thoughts @ 3am</a></h3>
       </div>
-    </div>
-    <div class="col-md-2">
     </div>
   </div>
   <br />
   <br />
   <div class="row">
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-3">
+    <div class="col-md-6">
       <div class="frontPageSection">
         <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/currently-consuming/">Currently Consuming</a></h3>
       </div>
     </div>
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-3">
+    <div class="col-md-6">
       <div class="frontPageSection">
         <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/obsessions/">Obsessions</a></h3>
       </div>
-    </div>
-    <div class="col-md-2">
     </div>
   </div>
 
