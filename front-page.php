018aa2a990e0980a1403c6a953a8cd98451c9439
Glen Knight <glenak1911@gmail.com>
2017-04-01 23:04:16 -0400

    started work on static homepage

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+  <div class="row">
+    <div class="col-md-6">
+      Little notes
+    </div>
+    <div class="col-md-6">
+      best thoughts @ 3am
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      Currently Consuming
+    </div>
+    <div class="col-md-6">
+      Obsessions
+    </div>
+  </div>
+
+<?php get_footer(); ?>
