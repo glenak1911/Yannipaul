7d05848ecfaeca4e81208c1cec681b844e9fa493
Glen Knight <glenak1911@gmail.com>
2017-04-03 00:13:18 -0400

    Added code to pull latest blog post

--- front-page.php
+++ front-page.php
@@ -2,7 +2,18 @@
   <div class="row">
     <div class="col-md-6">
       <div class="frontPageSection">
-        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/little-notes/">Little notes</a></h3>
+        <div class="frontPageHeaderContainer">
+          <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/little-notes/">Little notes</a></h3>
+        </div>
+        <div class="frontPagePost">
+          <?php
+            $args = array( 'numberposts' => 1 );
+            $lastposts = get_posts( $args );
+            foreach($lastposts as $post) : setup_postdata($post); ?>
+            	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            	<?php the_content(); ?>
+          <?php endforeach; ?>
+        </div>
       </div>
     </div>
     <div class="col-md-6">
