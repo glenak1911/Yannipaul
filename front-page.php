79b73d588e36c148a89864d5fc3cca75f8813ec5
Glen Knight <glenak1911@gmail.com>
2017-04-12 23:48:42 -0400

    Completed main page panels

--- front-page.php
+++ front-page.php
@@ -3,20 +3,35 @@
     <div class="col-md-6">
       <div class="frontPageSection">
         <?php
-          $args = array( 'numberposts' => 1 );
+          $args = array(
+            'numberposts' => 1,
+            'post_type' => _("little-notes")
+         );
           $lastposts = get_posts( $args );
           foreach($lastposts as $post) : setup_postdata($post); ?>
           	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           	<?php the_content(); ?>
         <?php endforeach; ?>
         <div class="frontPageHeaderContainer">
-          <div class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/little-notes/">Little notes</a></div>
+          <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/little-notes/">Little notes</a></h3>
         </div>
       </div>
     </div>
     <div class="col-md-6">
       <div class="frontPageSection">
-        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/best-thoughts/">Best thoughts @ 3am</a></h3>
+        <?php
+          $args = array(
+            'numberposts' => 1,
+            'post_type' => _("best-thoughts")
+         );
+          $lastposts = get_posts( $args );
+          foreach($lastposts as $post) : setup_postdata($post); ?>
+          	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          	<?php the_content(); ?>
+        <?php endforeach; ?>
+        <div class="frontPageHeaderContainer">
+          <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/best-thoughts/">Best Thoughts @ 3am</a></h3>
+        </div>
       </div>
     </div>
   </div>
@@ -25,12 +40,36 @@
   <div class="row">
     <div class="col-md-6">
       <div class="frontPageSection">
-        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/currently-consuming/">Currently Consuming</a></h3>
+        <?php
+          $args = array(
+            'numberposts' => 1,
+            'post_type' => _("currently-consuming")
+         );
+          $lastposts = get_posts( $args );
+          foreach($lastposts as $post) : setup_postdata($post); ?>
+          	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          	<?php the_content(); ?>
+        <?php endforeach; ?>
+        <div class="frontPageHeaderContainer">
+          <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/currently-consuming/">Currently Consuming</a></h3>
+        </div>
       </div>
     </div>
     <div class="col-md-6">
       <div class="frontPageSection">
-        <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/obsessions/">Obsessions</a></h3>
+        <?php
+          $args = array(
+            'numberposts' => 1,
+            'post_type' => _("obsessions")
+         );
+          $lastposts = get_posts( $args );
+          foreach($lastposts as $post) : setup_postdata($post); ?>
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_content(); ?>
+        <?php endforeach; ?>
+        <div class="frontPageHeaderContainer">
+          <h3 class="frontPageHeader"><a class="titleHeader" href="<?php bloginfo( 'url' );?>/obsessions/">Obsessions</a></h3>
+        </div>
       </div>
     </div>
   </div>
