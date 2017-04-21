83b4d15ca8c8bcc156fc032767201478204eb62a
Glen Knight <glenak1911@gmail.com>
2017-04-21 00:10:15 -0400

    Began code for little notes grid

--- page-little-notes.php
+++ page-little-notes.php
@@ -1,17 +1,55 @@
 <?php get_header(); ?>
 		<div class="col-md-8">
-			<?php
+				<?php
+				$counter = 1; //start counter
+
+				$grids = 2; //Grids per row
+
 				$args =  array(
-					'post_type' => 'little-notes',
-					'orderby' => 'menu_order',
-					'order' => 'ASC'
-				);
-				 $custom_query = new WP_Query( $args );
-            while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
-				<div class="blog-post">
-					<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<?php the_excerpt(); ?>
+			    'post_type' => 'little-notes',
+			    'orderby' => 'menu_order',
+			    'order' => 'ASC'
+			  );
+
+				global $query_string = new WP_Query( $args ); //Need this to make pagination work
+
+				/*Setting up our custom query (In here we are setting it to show 12 posts per page and eliminate all sticky posts*/
+				query_posts($query_string . '&caller_get_posts=1&posts_per_page=4');
+
+				if(have_posts()) :	while(have_posts()) :  the_post();
+				?>
+				<?php
+				//Show the left hand side column
+				if($counter == 1) :
+				?>
+				<div class="griditemleft">
+					<div class="postimage">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('category-thumbnail'); ?></a>
+					</div>
+	        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 				</div>
-				<?php endwhile; ?>
+				<?php
+				//Show the right hand side column
+				elseif($counter == $grids) :
+				?>
+				<div class="griditemright">
+					<div class="postimage">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('category-thumbnail'); ?></a>
+					</div>
+	        <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+				</div>
+				<div class="clear"></div>
+				<?php
+				$counter = 0;
+				endif;
+				?>
+				<?php
+					$counter++;
+					endwhile;
+					//Post Navigation code goes here
+					
+					endif;
+				?>
+			</div>
 		</div> <!-- /.col -->
 	<?php get_footer(); ?>
