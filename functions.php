ccde3c08625308a1d8f9469a9616416e8873ff00
Glen Knight <glenak1911@gmail.com>
2017-05-14 13:02:02 -0400

    Removed custom enqueued script

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@ function startwordpress_scripts() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6' );
 	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.6', true );
-  wp_enqueue_script( 'custom', get_template_directory_uri() . '/js/custom.js', array( 'jquery' ), '3.3.6', true );
   wp_enqueue_style( 'customStyle', get_template_directory_uri() . '/css/customStyle.css' );
 }
 
